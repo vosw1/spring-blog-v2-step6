97a342d6bf9b58e5659fa8d260fa823698211489
vosw1 <vosw1@naver.com>
2024-03-13 15:01:19 +0900

    user 더미 데이터 추가

--- src/main/java/shop/mtcoding/blog/board/Board.java
+++ src/main/java/shop/mtcoding/blog/board/Board.java
@@ -24,7 +24,7 @@ public class Board {
     // username은 user_tb에도 있기에 외래키로 연관관계 맺어야 함
     // @JoinColumn(name = "user_id") 직접 지정할 때
     @ManyToOne // ORM 할 것이기에 user 객체 필요
-    private User user; // user_id (변수명_PK키)
+    private User user; // DB에 컬럼명 : user_id (변수명_PK키)
 
     @CreationTimestamp // PC로 인해 DB에 INSERT될 때 날짜 주입
     private Timestamp createdAt;
