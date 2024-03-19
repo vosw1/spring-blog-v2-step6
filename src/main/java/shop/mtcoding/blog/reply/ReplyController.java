154367dc3a2163ad59327a9188744e36b93d12e9
vosw1 <vosw1@naver.com>
2024-03-19 16:22:49 +0900

    댓글 삭제하기

--- src/main/java/shop/mtcoding/blog/reply/ReplyController.java
+++ src/main/java/shop/mtcoding/blog/reply/ReplyController.java
@@ -3,6 +3,7 @@ package shop.mtcoding.blog.reply;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import shop.mtcoding.blog.user.User;
 
@@ -14,9 +15,16 @@ public class ReplyController {
     private final HttpSession session;
 
     @PostMapping("/reply/save")
-    public String save(ReplyRequest.SaveDTO reqDTO){
+    public String save(ReplyRequest.SaveDTO reqDTO) {
         User sessionUser = (User) session.getAttribute("sessionUser");
         replyService.save(reqDTO, sessionUser);
-        return "redirect:/board/"+reqDTO.getBoardId();
+        return "redirect:/board/" + reqDTO.getBoardId();
+    }
+
+    @PostMapping("/board/{boardId}/reply/{replyId}/delete")
+    public String delete(@PathVariable Integer boardId, @PathVariable Integer replyId) {
+        User sessionUser = (User) session.getAttribute("sessionUser");
+        replyService.delete(replyId, sessionUser.getId());
+        return "redirect:/board/" + boardId;
     }
 }
--- src/main/java/shop/mtcoding/blog/reply/ReplyService.java
+++ src/main/java/shop/mtcoding/blog/reply/ReplyService.java
@@ -3,6 +3,7 @@ package shop.mtcoding.blog.reply;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import shop.mtcoding.blog._core.errors.exception.Exception403;
 import shop.mtcoding.blog._core.errors.exception.Exception404;
 import shop.mtcoding.blog.board.Board;
 import shop.mtcoding.blog.board.BoardJPARepository;
@@ -14,6 +15,16 @@ public class ReplyService {
 
     private final BoardJPARepository boardJPARepository;
     private final ReplyJPARepository replyJPARepository;
+
+    @Transactional
+    public void delete(int boardId, Integer sessionUserId) {
+        Reply reply = replyJPARepository.findById(boardId)
+                .orElseThrow(() -> new Exception404("댓글을 찾을 수 없습니다"));
+        if (sessionUserId != reply.getUser().getId()) {
+            throw new Exception403("댓글을 삭제할 권한이 없습니다");
+        }
+        replyJPARepository.deleteById(boardId);
+    }
 
     @Transactional
     public void save(ReplyRequest.SaveDTO reqDTO, User sessionUser) {
